e820233d4b08334ba40c8ce4da3d41e20c8f4134
Aidan <survivalking4@gmail.com>
2022-09-30 14:53:17 -0700

    sfseth

--- register.js
+++ register.js
@@ -34,8 +34,8 @@ async function decrement(event) {
 }
 
 window.addEventListener("load", function() {
-    inputsArray = [count_20, count_10, count_5, count_1, count_q, count_d, count_n, count_p, count_under, count_envelope, count_other];
-    cashInputs = [count_20,count_10,count_5,count_1];
+    inputsArray = [count_20, count_10, count_5, count_1, count_q, count_d, count_n, count_p, count_under, count_envelope, count_other, count_big];
+    cashInputs = [count_20, count_10, count_5, count_1, count_big];
 });
 var inputsArray = [];
 var cashInputs = [];
